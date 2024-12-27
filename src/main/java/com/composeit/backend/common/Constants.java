fcc533204f126789504229f70de3e97a74a9f0a6
jblack527-git <jbb2000@hotmail.co.uk>
2024-12-27 20:02:47 +0000

    add flats to constants and fix up test

--- src/main/java/com/composeit/backend/common/Constants.java
+++ src/main/java/com/composeit/backend/common/Constants.java
@@ -2,15 +2,15 @@ package com.composeit.backend.common;
 
 public class Constants {
 	public static String C = "C";
-	public static String C_SHARP = "C#";
+	public static String C_SHARP = "C#/Db";
 	public static String D = "D";
-	public static String D_SHARP = "D#";
+	public static String D_SHARP = "D#/Eb";
 	public static String E = "E";
 	public static String F = "F";
-	public static String F_SHARP = "F#";
+	public static String F_SHARP = "F#/Gb";
 	public static String G = "G";
-	public static String G_SHARP = "G#";
+	public static String G_SHARP = "G#/Ab";
 	public static String A = "A";
-	public static String A_SHARP = "A#";
+	public static String A_SHARP = "A#/Bb";
 	public static String B = "B";
 }
