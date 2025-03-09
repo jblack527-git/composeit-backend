9930532dbd15080d4c913cbb1788c9358fe99f22
jblack527-git <jbb2000@hotmail.co.uk>
2025-03-09 12:58:14 +0000

    add full build package steps and rest mapping

---
+++ src/main/java/com/composeit/backend/controller/FrontendController.java
@@ -0,0 +1,13 @@
+package com.composeit.backend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FrontendController {
+
+    @GetMapping("/{path:[^\\.]*}")
+    public String forward() {
+        return "forward:/";
+    }
+}
